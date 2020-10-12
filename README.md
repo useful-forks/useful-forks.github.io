35d65cd98bcf58cd9d7db585ec0bac54992160cc
payne911 <payne911@users.noreply.github.com>
2020-10-12 16:01:24 -0400

    First increment : commits

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Chrome extension, adding a sorted list of starred forks to the GitHub forks page
 Not published, if you want to install it, enable developer mode in `chrome://extensions` and load the `project` folder as an unpacked extension.
 
 ![useful](https://user-images.githubusercontent.com/17261478/74049530-9a5e7f00-49d4-11ea-9162-d4ba35764302.PNG)
+
+## For devs
+
+The extension uses [content_scripts](https://developer.chrome.com/extensions/content_scripts#declaratively).
--- project/useful_forks.js
+++ project/useful_forks.js
@@ -1,78 +1,118 @@
+let USERNAME = ""
+let TOKEN = ""
+
 const svg_literal_fork = '<svg class="octicon octicon-repo-forked v-align-text-bottom" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 00-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 002 1a1.993 1.993 0 00-1 3.72V6.5l3 3v1.78A1.993 1.993 0 005 15a1.993 1.993 0 001-3.72V9.5l3-3V4.72A1.993 1.993 0 008 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>';
-
 const svg_literal_star = '<svg aria-label="star" height="16" class="octicon octicon-star v-align-text-bottom" viewBox="0 0 14 16" version="1.1" width="14" role="img"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>';
-
 const svg_literal_eye = '<svg class="octicon octicon-eye v-align-text-bottom" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>';
 
 const additional_css_literal = '#useful_forks_wrapper .repo div {display: inline-block;color: #666;margin: 2px 15px;} #useful_forks_wrapper .repo {text-align: center;}';
 
-function build_fork_element_html(combined_name, num_stars, num_watches, num_forks)
-{
-    return '<div class="repo"><div class="useful_forks_link">' + svg_literal_fork + '  <a href=' + combined_name + '"/">' + combined_name + '</a></div><div class="useful_forks_info">' + svg_literal_star + ' x ' + num_stars + ' | ' + svg_literal_eye + ' x ' + num_watches + ' | ' + svg_literal_fork + ' x ' + num_forks + '</div></div>';
+function extract_username_from_fork(combined_name) {
+  return combined_name.split('/')[0];
 }
 
-function add_fork_elements(forkdata_array)
-{
-    if (!forkdata_array || forkdata_array.length == 0)
-        return;
+function badget_width(number) {
+  return 70 * number.toString().length; // magic number 70 extracted from querying 'shields.io'
+}
 
-    let wrapper_html = '<h5>Starred Forks</h5>';
-    for (let i = 0; i < Math.min(15, forkdata_array.length); ++i)
-    {
-        const elem_ref = forkdata_array[i];
-        wrapper_html += build_fork_element_html(elem_ref.full_name, elem_ref.stargazers_count, elem_ref.watchers_count, elem_ref.forks_count);
-    }
+function ahead_badge(amount) {
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="88" height="18" role="img"><title>ahead</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-color="#000" stop-opacity=".3"/><stop offset="1" stop-color="#000" stop-opacity=".5"/></linearGradient><clipPath id="r"><rect width="88" height="18" rx="4" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="43" height="18" fill="#555"/><rect x="43" width="45" height="18" fill="#007ec6"/><rect width="88" height="18" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="225" y="140" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="330">ahead</text><text x="225" y="130" transform="scale(.1)" fill="#fff" textLength="330">ahead</text><text x="645" y="130" transform="scale(.1)" fill="#fff" textLength="' + badget_width(amount) + '">' + amount + '</text></g></svg>';
+}
 
-    wrapper_html += '<br><br>';
-    
-    let old_wrapper = document.getElementById('useful_forks_wrapper');
-    if (old_wrapper)
-    {
-        old_wrapper.remove();
-    }
+function behind_badge(amount) {
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="92" height="18" role="img"><title>behind</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-color="#000" stop-opacity=".3"/><stop offset="1" stop-color="#000" stop-opacity=".5"/></linearGradient><clipPath id="r"><rect width="92" height="18" rx="4" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="47" height="18" fill="#555"/><rect x="47" width="45" height="18" fill="#007ec6"/><rect width="92" height="18" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="245" y="140" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="370">behind</text><text x="245" y="130" transform="scale(.1)" fill="#fff" textLength="370">behind</text><text x="685" y="130" transform="scale(.1)" fill="#fff" textLength="' + badget_width(amount) + '">' + amount + '</text></g></svg>';
+}
 
-    let new_wrapper = document.createElement('div');
-    new_wrapper.setAttribute("id", "useful_forks_wrapper");
-    new_wrapper.innerHTML = wrapper_html;
-    document.getElementById('network').prepend(new_wrapper);
+function build_fork_element_html(combined_name, num_stars, num_watches, num_forks) {
+  return '<tr id="' + extract_username_from_fork(combined_name) + '"><div class="repo">'
+      + '<td><div class="useful_forks_link">' + svg_literal_fork + ' <a href=https://github.com/' + combined_name + '>' + combined_name + '</a></div></td>'
+      + '<td><div class="useful_forks_info">' + svg_literal_star + ' x ' + num_stars + ' | ' + svg_literal_eye + ' x ' + num_watches + ' | ' + svg_literal_fork + ' x ' + num_forks + '</div></td>'
+      + '</div></tr>';
+}
+
+function commits_count(response, fork_username) {
+  return () => {
+    response = JSON.parse(response.responseText);
+    let old_data = document.getElementById(fork_username);
+    let appendedData = old_data.insertCell();
+    appendedData.innerHTML = '<div class="useful_forks_commits">'
+        + '&nbsp;| ' + ahead_badge(response.ahead_by)
+        + '&nbsp;| ' + behind_badge(response.behind_by)
+        + '&nbsp;| total_commits=' + response.total_commits
+        + '</div>';
+  }
+}
+
+function authenticatedRequestFactory(url) {
+  let request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.setRequestHeader("Accept", "application/vnd.github.v3+json");
+  request.setRequestHeader("Authorization", "Basic " + btoa(USERNAME + ":" + TOKEN));
+  return request;
+}
+
+function add_fork_elements(forkdata_array, user, repo) {
+  if (!forkdata_array || forkdata_array.length === 0)
+    return;
+
+  console.log(forkdata_array);
+
+  let wrapper_html = '<h5>Useful forks</h5><table>';
+
+  for (let i = 0; i < Math.min(100, forkdata_array.length); ++i) {
+    const elem_ref = forkdata_array[i];
+
+    wrapper_html += build_fork_element_html(elem_ref.full_name, elem_ref.stargazers_count, elem_ref.watchers_count, elem_ref.forks_count);
+
+    const fork_username = extract_username_from_fork(elem_ref.full_name);
+    let request = authenticatedRequestFactory('https://api.github.com/repos/' + user + '/' + repo + '/compare/master...' + fork_username + ':master');
+    request.onreadystatechange = onreadystatechangeFactory(request, commits_count(request, fork_username));
+    request.send();
+  }
+
+  wrapper_html += '</table><br/><br/>';
+
+  let old_wrapper = document.getElementById('useful_forks_wrapper');
+  if (old_wrapper)
+    old_wrapper.remove();
+
+  let new_wrapper = document.createElement('div');
+  new_wrapper.setAttribute("id", "useful_forks_wrapper");
+  new_wrapper.innerHTML = wrapper_html;
+  document.getElementById('network').prepend(new_wrapper);
 }
 
 function onreadystatechangeFactory(xhr, successFn) {
-    return function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                successFn();
-            } else if (xhr.status === 403) {
-                console.warn('Looks like the rate-limit was exceeded.');
-            } else {
-                console.warn('GitHub API returned status:', xhr.status);
-            }
-        } else {
-            // Request is still in progress
-        }
-    };
+  return () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        successFn();
+      } else if (xhr.status === 403) {
+        console.warn('Looks like the rate-limit was exceeded.');
+      } else {
+        console.warn('GitHub API returned status:', xhr.status);
+      }
+    } else {
+      // Request is still in progress
+    }
+  };
 }
 
 function load_useful_forks(user, repo) {
-    var styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = additional_css_literal;
-    document.head.appendChild(styleSheet);
+  let styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = additional_css_literal;
+  document.head.appendChild(styleSheet);
 
-    var xhrFork = new XMLHttpRequest();
-    xhrFork.onreadystatechange = onreadystatechangeFactory(
-        xhrFork,
-        function () {
-            add_fork_elements(JSON.parse(xhrFork.responseText));
-        }
-    );
-
-    xhrFork.open('GET', 'https://api.github.com/repos/' + user + '/' + repo + '/forks?sort=stargazers');
-    xhrFork.send();
+  let request = authenticatedRequestFactory('https://api.github.com/repos/' + user + '/' + repo + '/forks?sort=stargazers&per_page=100')
+  request.onreadystatechange = onreadystatechangeFactory(request,
+      () => add_fork_elements(JSON.parse(request.responseText), user, repo)
+  );
+  request.send();
 }
 
 const pathComponents = window.location.pathname.split('/');
 if (pathComponents.length >= 3) {
-    const user = pathComponents[1], repo = pathComponents[2];
-    load_useful_forks(user, repo);
+  const user = pathComponents[1], repo = pathComponents[2];
+  load_useful_forks(user, repo);
 }
