9a9ffcba264517e19f07960a69a8e1b778573042
payne911 <payne911@users.noreply.github.com>
2020-10-20 01:29:19 -0400

    Website advancements

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
+![<img src="media/useful-forks-logo.png" width="250"/>](media/useful-forks-logo.png)
+
 # Useful Forks
-Chrome extension, adding a sorted list of starred forks to the GitHub forks page. Results are filtered out if there were never any commits pushed since the fork was created.
+Adding a sorted list of starred forks to the GitHub forks page. Results are filtered out if there were never any commits pushed since the fork was created.
 
-Not published, if you want to install it, enable `Developer mode` (switch at the top-right) at the `chrome://extensions` URL and load the `project` folder as an unpacked extension (`Load unpacked` button at the top-left).
+The website form is great for people that just want to use this tool every now and then.
 
-You should change the [script file](project/useful_forks.js) so that it includes your GitHub username and an [Access Token](https://github.com/settings/tokens/new?scopes=repo&description=UsefulFork).
+The Chrome Extension has the advantage of always performing the filtering whenever you open a project's GitHub Forks page.
+
+## Website (WIP)
+The project is [available online](https://payne911.github.io/useful-forks/) thanks to GitHub Pages.
+
+## Chrome Extension (WIP)
+Not published yet. If you want to install it:
+1. Clone this repository
+2. Change the [script file](project/useful_forks.js) so that it includes your GitHub username and an [Access Token](https://github.com/settings/tokens/new?scopes=repo&description=UsefulFork).
+3. Go to the `chrome://extensions` URL
+4. Enable `Developer mode` (switch at the top-right)
+5. Click `Load unpacked` (button at the top-left)
+6. Select the [`project` folder](project)
+
+Once it's activated, the extension will automatically trigger itself whenever you open a Fork page, as shown by the GIF below:
 
 ![example](media/demo.gif)
 
-## For devs
-The extension uses [content_scripts](https://developer.chrome.com/extensions/content_scripts#declaratively).
-
-## To-do
-* Sort by Star (a proper one!)
+# To-do
+* Finish the website
+* Sorting algos
 * Cache responses for a little while 
   * https://docs.github.com/en/free-pro-team@latest/rest/guides/getting-started-with-the-rest-api#conditional-requests
   * https://docs.github.com/en/free-pro-team@latest/rest/overview/resources-in-the-rest-api#conditional-requests
@@ -20,4 +34,7 @@ The extension uses [content_scripts](https://developer.chrome.com/extensions/con
   * See: https://developer.chrome.com/extensions/samples
     * https://developer.chrome.com/extensions/examples/tutorials/broken_background_color.zip
     * https://developer.chrome.com/extensions/examples/tutorials/getstarted.zip
-* Allow people to input their Access Token to increase API limits (from Extension's settings rather than through script)
+  * Allow people to input their Access Token to increase API limits (from Extension's settings rather than through script)
+
+# Credits
+Thanks to [raeleus](https://github.com/raeleus) for his design of the logo!
--- docs/src/index.js
+++ docs/src/index.js
@@ -1,3 +1,6 @@
+const GITHUB_ACCESS_TOKEN = "useful-forks-access-token";
+
+
 function status(response) {
   if (response.status >= 200 && response.status < 300) {
     return Promise.resolve(response)
@@ -17,3 +20,125 @@ document.querySelector('#theBtn').addEventListener('click',
     .then(status)
     .then(json)
     .then(response => console.log(response)));
+
+// // import getStarHistory from '../../core/getStarHistory';
+// // import draw from './draw';
+
+let data = [];
+
+let token = localStorage.getItem(GITHUB_ACCESS_TOKEN);
+drawAddTokenBtn(token);
+
+// async function getRepoNameFetchAndDraw() {
+//   // get repo str (format: 'timqian/star-history')
+//   let repo = ''
+//   let rawRepoStr = document.getElementById('repo').value;
+//   if (rawRepoStr.includes('github.com')) {
+//     rawRepoStr += '\/'      // make sure url end with /
+//     repo = /github.com\/(\S*?\/\S*?)[\/#?]/.exec(rawRepoStr)[1];
+//   } else {
+//     repo = rawRepoStr == '' ? 'timqian/star-history' : rawRepoStr;
+//   }
+//
+//   for (let item of data) {
+//     if (item.label == repo) {
+//       notie.alert({ text: 'This repo is already on the chart', type: 'warning' });
+//       return;
+//     }
+//   }
+//
+//   token = localStorage.getItem(GITHUB_ACCESS_TOKEN);
+//   await fetchDataAndDraw(repo, token);
+// }
+
+
+document.getElementById('theBtn').addEventListener('click', async event => {
+  event.preventDefault();
+  console.log("PRESSED THE BUTTON"); // todo
+  // await getRepoNameFetchAndDraw();
+});
+
+document.querySelector('#repo').addEventListener('keyup', async (e) => {
+  if (e.keyCode === 13) {
+    console.log("PRESSED ENTER"); // todo
+  }
+});
+
+const localToken = localStorage.getItem(GITHUB_ACCESS_TOKEN);
+if (localToken) {
+  document.getElementById('tokenInput').value = localToken;
+}
+
+document.getElementById('addTokenBtn').addEventListener('click', event => {
+  event.preventDefault();
+  document.querySelector('.modal').classList.add('is-active');
+});
+
+document.getElementById('closeModalBtn').addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector('.modal').classList.remove('is-active');
+});
+
+document.getElementById('saveTokenBtn').addEventListener('click', e => {
+  e.preventDefault();
+  const token = document.getElementById('tokenInput').value;
+  localStorage.setItem(GITHUB_ACCESS_TOKEN, token);
+  document.querySelector('.modal').classList.remove('is-active');
+  drawAddTokenBtn(token);
+});
+
+function drawAddTokenBtn(token) {
+  let verb = 'Add';
+  if (token) {
+    verb = 'Edit'
+  }
+  document.getElementById('addTokenBtn').innerHTML = '<strong>' + verb + ' Access Token</strong>';
+  document.getElementById('modalCardTitle').innerHTML = verb + ' GitHub Access Token';
+}
+
+// async function fetchDataAndDraw(repo, token) {
+//
+//   document.getElementById('theBtn').setAttribute("disabled", "disabled");
+//   document.getElementById('theBtn').classList.add('is-loading');
+//
+//   try {
+//     const starHistory = await getStarHistory(repo, token);
+//     // new data
+//     data.push({
+//       label: repo,
+//       data: starHistory.map((item) => {
+//         return {
+//           x: new Date(item.date),
+//           y: Number(item.starNum)
+//         }
+//       }),
+//     });
+//
+//     draw(data);
+//
+//     if (location.hash === '') {
+//       location.hash += repo;
+//     } else if (location.hash.length >=3 && !location.hash.includes(repo)) { // minimal sample of repo name 'a/b'
+//
+//       location.hash += '&' + repo;
+//     }
+//   } catch (error) {
+//     console.dir(error);
+//     if (error.response.status === 403) {
+//       notie.alert({ text: 'GitHub API rate limit exceeded', type:'warning' });
+//
+//       setTimeout(() => {
+//         document.querySelector('.modal').classList.add('is-active');
+//       }, 2500);
+//     } else if (error.response.status === 404) {
+//       notie.alert({text:'No such repo', type:'warning' })
+//     } else {
+//       notie.alert({text:'Some unexpected error happened, try again', type:'warning' })
+//     }
+//   }
+//
+//   document.getElementById('theBtn').removeAttribute("disabled");
+//   document.getElementById('theBtn').classList.remove('is-loading');
+//   document.getElementById('button-group').style.visibility = 'visible';
+//   document.getElementById('chart').style.visibility = 'visible';
+// }
