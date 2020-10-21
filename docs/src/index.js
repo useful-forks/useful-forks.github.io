3d50ab9845154d4f7914459bf7d9fe4d47d1337e
payne911 <payne911@users.noreply.github.com>
2020-10-21 19:13:58 -0400

    WEBSITE: first working version

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,146 +1,80 @@
-const GITHUB_ACCESS_TOKEN = "useful-forks-access-token";
-
-
-function status(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  } else {
-    return Promise.reject(new Error(response.statusText))
-  }
-}
-
-function json(response) {
-  return response.json()
-}
-
-// for headers:  https://stackoverflow.com/a/45753864/9768291
-document.querySelector('#theBtn').addEventListener('click',
-    () => fetch('https://api.github.com/repos/payne911/PieMenu/forks?sort=stargazers&per_page=100&page=1',
-        {mode: 'cors'})
-    .then(status)
-    .then(json)
-    .then(response => console.log(response)));
-
-// // import getStarHistory from '../../core/getStarHistory';
-// // import draw from './draw';
-
-let data = [];
-
-let token = localStorage.getItem(GITHUB_ACCESS_TOKEN);
+// Gather the saved Access Token
+const GITHUB_ACCESS_TOKEN_STORAGE_KEY = "useful-forks-access-token";
+let token = localStorage.getItem(GITHUB_ACCESS_TOKEN_STORAGE_KEY);
 drawAddTokenBtn(token);
 
-// async function getRepoNameFetchAndDraw() {
-//   // get repo str (format: 'timqian/star-history')
-//   let repo = ''
-//   let rawRepoStr = document.getElementById('repo').value;
-//   if (rawRepoStr.includes('github.com')) {
-//     rawRepoStr += '\/'      // make sure url end with /
-//     repo = /github.com\/(\S*?\/\S*?)[\/#?]/.exec(rawRepoStr)[1];
-//   } else {
-//     repo = rawRepoStr == '' ? 'timqian/star-history' : rawRepoStr;
-//   }
-//
-//   for (let item of data) {
-//     if (item.label == repo) {
-//       notie.alert({ text: 'This repo is already on the chart', type: 'warning' });
-//       return;
-//     }
-//   }
-//
-//   token = localStorage.getItem(GITHUB_ACCESS_TOKEN);
-//   await fetchDataAndDraw(repo, token);
-// }
+// Initialize the structure used by the 'queries-logic.js'
+$('#useful_forks_inject').append(
+    $('<div>', {id: UF_ID_WRAPPER}).append(
+        $('<br>'),
+        $('<div>', {id: UF_ID_MSG}),
+        $('<div>', {id: UF_ID_DATA}).append(
+            $('<table>', {id: UF_ID_TABLE}).append(
+                $('<tbody>')
+            )
+        )
+    )
+);
 
 
-document.getElementById('theBtn').addEventListener('click', async event => {
+function disable_btn() {
+  const SEARCH_BTN = $('#searchBtn');
+  if (SEARCH_BTN.hasClass('is-loading')) {
+    return; // abort
+  }
+  SEARCH_BTN.addClass('is-loading');
+}
+
+/** Extracts 'user' and 'repo' values from potential URL inputs. */
+function initiate_search() {
+  let values = $('#repo').val().split('/').filter(Boolean);
+  let len = values.length;
+
+  if (len < 1) {
+    $('#' + UF_ID_MSG).html('Please enter a valid query: it should contain two strings separated by a "/"');
+    return; // abort
+  }
+
+  disable_btn();
+  request_fork_page(1, values[len-2], values[len-1], token);
+}
+
+$('#searchBtn').click(event => {
   event.preventDefault();
-  console.log("PRESSED THE BUTTON"); // todo
-  // await getRepoNameFetchAndDraw();
+  initiate_search();
 });
 
-document.querySelector('#repo').addEventListener('keyup', async (e) => {
-  if (e.keyCode === 13) {
-    console.log("PRESSED ENTER"); // todo
+$('#repo').keyup(event => {
+  if (event.keyCode === 13) { // only when pressing 'ENTER'
+    initiate_search();
   }
 });
 
-const localToken = localStorage.getItem(GITHUB_ACCESS_TOKEN);
-if (localToken) {
-  document.getElementById('tokenInput').value = localToken;
-}
-
-document.getElementById('addTokenBtn').addEventListener('click', event => {
+$('#addTokenBtn').click(event => {
   event.preventDefault();
-  document.querySelector('.modal').classList.add('is-active');
+  $('.modal').addClass('is-active');
 });
 
-document.getElementById('closeModalBtn').addEventListener('click', e => {
-  e.preventDefault();
-  document.querySelector('.modal').classList.remove('is-active');
+$('#closeModalBtn').click(event => {
+  event.preventDefault();
+  $('.modal').removeClass('is-active');
 });
 
-document.getElementById('saveTokenBtn').addEventListener('click', e => {
-  e.preventDefault();
-  const token = document.getElementById('tokenInput').value;
-  localStorage.setItem(GITHUB_ACCESS_TOKEN, token);
-  document.querySelector('.modal').classList.remove('is-active');
-  drawAddTokenBtn(token);
+$('#saveTokenBtn').click(event => {
+  event.preventDefault();
+  const INPUT_TOKEN = document.getElementById('tokenInput').value;
+  localStorage.setItem(GITHUB_ACCESS_TOKEN_STORAGE_KEY, INPUT_TOKEN);
+  $('.modal').removeClass('is-active');
+  drawAddTokenBtn(INPUT_TOKEN);
 });
 
 function drawAddTokenBtn(accessToken) {
   let verb = 'Add';
   if (accessToken) {
     verb = 'Edit'
+    $('#tokenInput').val(accessToken);
   }
-  const tokenBtn = '<img src="assets/settings-icon.png" alt="settings" />'
-      + '<strong>&nbsp;&nbsp;' + verb + ' Access Token</strong>'
-  document.getElementById('addTokenBtn').innerHTML = tokenBtn;
-  document.getElementById('modalCardTitle').innerHTML = verb + ' GitHub Access Token';
+  $('#addTokenBtn').html('<img src="assets/settings-icon.png" alt="settings" />'
+      + '<strong>&nbsp;&nbsp;' + verb + ' Access Token</strong>');
+  $('#modalCardTitle').html(verb + ' GitHub Access Token');
 }
-
-// async function fetchDataAndDraw(repo, token) {
-//
-//   document.getElementById('theBtn').setAttribute("disabled", "disabled");
-//   document.getElementById('theBtn').classList.add('is-loading');
-//
-//   try {
-//     const starHistory = await getStarHistory(repo, token);
-//     // new data
-//     data.push({
-//       label: repo,
-//       data: starHistory.map((item) => {
-//         return {
-//           x: new Date(item.date),
-//           y: Number(item.starNum)
-//         }
-//       }),
-//     });
-//
-//     draw(data);
-//
-//     if (location.hash === '') {
-//       location.hash += repo;
-//     } else if (location.hash.length >=3 && !location.hash.includes(repo)) { // minimal sample of repo name 'a/b'
-//
-//       location.hash += '&' + repo;
-//     }
-//   } catch (error) {
-//     console.dir(error);
-//     if (error.response.status === 403) {
-//       notie.alert({ text: 'GitHub API rate limit exceeded', type:'warning' });
-//
-//       setTimeout(() => {
-//         document.querySelector('.modal').classList.add('is-active');
-//       }, 2500);
-//     } else if (error.response.status === 404) {
-//       notie.alert({text:'No such repo', type:'warning' })
-//     } else {
-//       notie.alert({text:'Some unexpected error happened, try again', type:'warning' })
-//     }
-//   }
-//
-//   document.getElementById('theBtn').removeAttribute("disabled");
-//   document.getElementById('theBtn').classList.remove('is-loading');
-//   document.getElementById('button-group').style.visibility = 'visible';
-//   document.getElementById('chart').style.visibility = 'visible';
-// }
---
+++ docs/src/queries-logic.js
@@ -0,0 +1,202 @@
+let GITHUB_ACCESS_TOKEN   = ""
+let INITIAL_QUERY_USER    = "" // todo: reinitialize for next query
+
+const UF_ID_WRAPPER = 'useful_forks_wrapper';
+const UF_ID_MSG     = 'useful_forks_msg';
+const UF_ID_DATA    = 'useful_forks_data';
+const UF_ID_TABLE   = 'useful_forks_table';
+
+const svg_literal_fork = '<svg class="octicon octicon-repo-forked v-align-text-bottom" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 00-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 002 1a1.993 1.993 0 00-1 3.72V6.5l3 3v1.78A1.993 1.993 0 005 15a1.993 1.993 0 001-3.72V9.5l3-3V4.72A1.993 1.993 0 008 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>';
+const svg_literal_star = '<svg aria-label="star" height="16" class="octicon octicon-star v-align-text-bottom" viewBox="0 0 14 16" version="1.1" width="14" role="img"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>';
+const svg_literal_eye  = '<svg class="octicon octicon-eye v-align-text-bottom" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8.06 2C3 2 0 8 0 8s3 6 8.06 6C13 14 16 8 16 8s-3-6-7.94-6zM8 12c-2.2 0-4-1.78-4-4 0-2.2 1.8-4 4-4 2.22 0 4 1.8 4 4 0 2.22-1.78 4-4 4zm2-4c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>';
+
+const UF_MSG_NO_FORKS     = "No one forked this specific repository.";
+const UF_MSG_SCANNING     = "Currently scanning all the forks.";
+const UF_MSG_EMPTY_FILTER = "All the forks have been filtered out: you can now rest easy!";
+const UF_MSG_API_RATE     = "<b>Exceeded GitHub API rate-limits.</b> Consider providing an <b>Access Token</b>.";
+const UF_TABLE_SEPARATOR  = "&nbsp;|&nbsp;";
+
+const FORKS_PER_PAGE = 100; // enforced by GitHub API
+
+
+function extract_username_from_fork(combined_name) {
+  return combined_name.split('/')[0];
+}
+
+function badge_width(number) {
+  return 70 * number.toString().length; // magic number 70 extracted from querying 'shields.io'
+}
+
+/** Credits to https://shields.io/ */
+function ahead_badge(amount) {
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="88" height="18" role="img"><title>How far ahead of the original repo\'s master branch this fork\'s master branch is</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-color="#000" stop-opacity=".3"/><stop offset="1" stop-color="#000" stop-opacity=".5"/></linearGradient><clipPath id="r"><rect width="88" height="18" rx="4" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="43" height="18" fill="#555"/><rect x="43" width="45" height="18" fill="#007ec6"/><rect width="88" height="18" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="225" y="140" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="330">ahead</text><text x="225" y="130" transform="scale(.1)" fill="#fff" textLength="330">ahead</text><text x="645" y="130" transform="scale(.1)" fill="#fff" textLength="' + badge_width(amount) + '">' + amount + '</text></g></svg>';
+}
+
+/** Credits to https://shields.io/ */
+function behind_badge(amount) {
+  const color = amount === 0 ? '#4c1' : '#007ec6'; // green only when not behind, blue otherwise
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="92" height="18" role="img"><title>How far behind of the original repo\'s master branch this fork\'s master branch is</title><linearGradient id="s" x2="0" y2="100%"><stop offset="0" stop-color="#fff" stop-opacity=".7"/><stop offset=".1" stop-color="#aaa" stop-opacity=".1"/><stop offset=".9" stop-color="#000" stop-opacity=".3"/><stop offset="1" stop-color="#000" stop-opacity=".5"/></linearGradient><clipPath id="r"><rect width="92" height="18" rx="4" fill="#fff"/></clipPath><g clip-path="url(#r)"><rect width="47" height="18" fill="#555"/><rect x="47" width="45" height="18" fill="'+ color +'"/><rect width="92" height="18" fill="url(#s)"/></g><g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="245" y="140" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="370">behind</text><text x="245" y="130" transform="scale(.1)" fill="#fff" textLength="370">behind</text><text x="685" y="130" transform="scale(.1)" fill="#fff" textLength="' + badge_width(amount) + '">' + amount + '</text></g></svg>';
+}
+
+function getElementById_$(id) {
+  return $('#' + id);
+}
+
+function getTdValue(rows, index, col) {
+  return rows.item(index).getElementsByTagName('td').item(col).getAttribute("value");
+}
+
+/** 'sortColumn' index starts at 0.   https://stackoverflow.com/a/37814596/9768291 */
+function sortTable(table_id, sortColumn){
+  let tableData = document.getElementById(table_id).getElementsByTagName('tbody').item(0);
+  let rows = tableData.getElementsByTagName('tr');
+  for(let i = 0; i < rows.length - 1; i++) {
+    for(let j = 0; j < rows.length - (i + 1); j++) {
+      if(getTdValue(rows, j, sortColumn) < getTdValue(rows, j+1, sortColumn)) {
+        tableData.insertBefore(rows.item(j+1), rows.item(j));
+      }
+    }
+  }
+}
+
+/** The secondary request which appends the badges. */
+function commits_count(request, table_body, fork_username) {
+  return () => {
+    const response = JSON.parse(request.responseText);
+
+    let old_data = getElementById_$(fork_username);
+    if (response.total_commits === 0) {
+      old_data.remove();
+      if (table_body.children().length === 0) {
+        getElementById_$(UF_ID_MSG).html(UF_MSG_EMPTY_FILTER);
+      }
+    } else {
+      old_data.append(
+          $('<td>').html(UF_TABLE_SEPARATOR),
+          $('<td>').html(ahead_badge(response.ahead_by)),
+          $('<td>').html(UF_TABLE_SEPARATOR),
+          $('<td>').html(behind_badge(response.behind_by))
+      )
+    }
+  }
+}
+
+/** To remove erroneous repos. */
+function commits_count_failure(fork_username) {
+  return () => {
+    getElementById_$(fork_username).remove();
+  }
+}
+
+/** To use the Access Token with a request. */
+function authenticatedRequestHeaderFactory(url) {
+  let request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.setRequestHeader("Accept", "application/vnd.github.v3+json");
+  if (GITHUB_ACCESS_TOKEN) {
+    request.setRequestHeader("Authorization", "token " + GITHUB_ACCESS_TOKEN);
+  }
+  return request;
+}
+
+function onreadystatechangeFactory(xhr, successFn, failureFn) {
+  return () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        successFn();
+      } else if (xhr.status === 403) {
+        console.warn('Looks like the rate-limit was exceeded.');
+        getElementById_$(UF_ID_MSG).html(UF_MSG_API_RATE);
+        // todo: open the Access Token dialog?
+      } else {
+        console.warn('GitHub API returned status:', xhr.status);
+        failureFn();
+      }
+    } else {
+      // Request is still in progress
+    }
+  };
+}
+
+/** Dynamically fills the second part of the rows. */
+function build_fork_element_html(table_body, combined_name, num_stars, num_watches, num_forks) {
+  table_body.append(
+      $('<tr>', {id: extract_username_from_fork(combined_name), class: "useful_forks_repo"}).append(
+          $('<td>').html(svg_literal_fork + ' <a href=https://github.com/' + combined_name + '>' + combined_name + '</a>'),
+          $('<td>').html(UF_TABLE_SEPARATOR + svg_literal_star + ' x ' + num_stars).attr("value", num_stars),
+          $('<td>').html(UF_TABLE_SEPARATOR + svg_literal_eye + ' x ' + num_watches).attr("value", num_watches),
+          $('<td>').html(UF_TABLE_SEPARATOR + svg_literal_fork + ' x ' + num_forks).attr("value", num_forks)
+      )
+  );
+}
+
+/** Prepares, appends, and updates dynamically a table row. */
+function add_fork_elements(forkdata_array, user, repo) {
+  if (!forkdata_array || forkdata_array.length === 0)
+    return;
+
+  getElementById_$(UF_ID_MSG).html("");
+  let table_body = getElementById_$(UF_ID_TABLE).find("tbody");
+
+  for (let i = 0; i < Math.min(FORKS_PER_PAGE, forkdata_array.length); ++i) {
+    const elem_ref = forkdata_array[i];
+
+    /* Basic data (stars, watchers, forks). */
+    build_fork_element_html(table_body, elem_ref.full_name, elem_ref.stargazers_count, elem_ref.watchers_count, elem_ref.forks_count);
+
+    /* Commits diff data (ahead/behind). */
+    const fork_username = extract_username_from_fork(elem_ref.full_name);
+    let request = authenticatedRequestHeaderFactory('https://api.github.com/repos/' + user + '/' + repo + '/compare/master...' + fork_username + ':master');
+    request.onreadystatechange = onreadystatechangeFactory(request, commits_count(request, table_body, fork_username), commits_count_failure(fork_username));
+    request.send();
+
+    /* Forks of forks. */
+    if (elem_ref.forks_count > 0) {
+      request_fork_page(1, elem_ref.owner.login, elem_ref.name);
+    }
+  }
+}
+
+/** Paginated request. Pages index start at 1. */
+function request_fork_page(page_number, user, repo, token) {
+  if (token) {
+    GITHUB_ACCESS_TOKEN = token;
+  }
+  if (!INITIAL_QUERY_USER) {
+    INITIAL_QUERY_USER = user;
+  }
+
+  if (page_number === 1 && INITIAL_QUERY_USER === user) {
+    getElementById_$(UF_ID_MSG).html(UF_MSG_SCANNING);
+  }
+
+  let request = authenticatedRequestHeaderFactory('https://api.github.com/repos/' + user + '/' + repo + '/forks?sort=stargazers&per_page=' + FORKS_PER_PAGE + '&page=' + page_number)
+  request.onreadystatechange = onreadystatechangeFactory(request,
+      () => {
+        const response = JSON.parse(request.responseText);
+        if (!response || response.length === 0) {
+          if (page_number === 1) {
+            getElementById_$(UF_ID_MSG).html(UF_MSG_NO_FORKS);
+          }
+          return;
+        }
+
+        /* Pagination (beyond 100 forks). */
+        const link_header = request.getResponseHeader("link");
+        if (link_header) {
+          let contains_next_page = link_header.indexOf('>; rel="next"');
+          if (contains_next_page !== -1) {
+            request_fork_page(++page_number, user, repo);
+          }
+        }
+
+        /* Half-assed sort. (todo: redo this) */
+        $( () => {
+          sortTable(UF_ID_TABLE, 1);
+        });
+
+        /* Populate the table. */
+        add_fork_elements(response, user, repo);
+      });
+  request.send();
+}
