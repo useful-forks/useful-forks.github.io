6c80270a312a56b17cbf968c73748ac2d058f9c5
odnar-dev <73726132+odnar-dev@users.noreply.github.com>
2021-03-12 16:57:12 +0100

    Update script.user.js
    fixed : https://github.com/useful-forks/useful-forks.github.io/issues/9
    fixed : https://github.com/useful-forks/useful-forks.github.io/issues/4
    only call if GITHUB_ACCESS_TOKEN has been set up or show an error

--- userscript/script.user.js
+++ userscript/script.user.js
@@ -1,4 +1,42 @@
-let GITHUB_ACCESS_TOKEN = ""
+// ==UserScript==
+// @name        Useful Forks
+// @author      useful-forks
+// @version     1.5
+// @namespace   https://github.com/useful-forks
+// @description Displays GitHub forks ordered by stars, and with additional information and automatic filters.
+// @match       *://github.com/*/*
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_setValue
+// @grant       GM_openInTab
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
+// @icon        https://useful-forks.github.io/assets/useful-forks-logo.png
+// @homepageURL https://github.com/useful-forks/useful-forks.github.io
+// @supportURL  https://github.com/useful-forks/useful-forks.github.io/issues
+// ==/UserScript==
+(function() {
+
+let GITHUB_ACCESS_TOKEN = GM_getValue('GITHUB_ACCESS_TOKEN')
+
+GM_registerMenuCommand("Set Github Access Token", setPersonalToken)
+GM_registerMenuCommand("Generate New Access Token", newPersonalToken);
+
+function setPersonalToken(){
+    var mess = "Personal Access Token";
+    var caseShow = GITHUB_ACCESS_TOKEN;
+    var getpersonalToken = prompt(mess, caseShow);
+    GITHUB_ACCESS_TOKEN = (getpersonalToken===null? GITHUB_ACCESS_TOKEN : getpersonalToken)
+    GM_setValue("GITHUB_ACCESS_TOKEN", GITHUB_ACCESS_TOKEN)
+}
+
+function newPersonalToken(){
+  let tabControl = GM_openInTab("https://github.com/settings/tokens/new?scopes=repo&description=UsefulFork")
+  tabControl.onclose = () => setPersonalToken();
+}
+
+function valid(string) {
+  return string && string.length > 0;
+}
 
 const UF_ID_WRAPPER = 'useful_forks_wrapper';
 const UF_ID_TITLE   = 'useful_forks_title';
@@ -9,12 +47,40 @@ const UF_ID_TABLE   = 'useful_forks_table';
 const svg_literal_fork = '<svg class="octicon octicon-repo-forked v-align-text-bottom" viewBox="0 0 10 16" version="1.1" width="10" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 1a1.993 1.993 0 00-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 002 1a1.993 1.993 0 00-1 3.72V6.5l3 3v1.78A1.993 1.993 0 005 15a1.993 1.993 0 001-3.72V9.5l3-3V4.72A1.993 1.993 0 008 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>';
 const svg_literal_star = '<svg aria-label="star" height="16" class="octicon octicon-star v-align-text-bottom" viewBox="0 0 14 16" version="1.1" width="14" role="img"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>';
 
+function getSyntaxTheme() {
+  switch (document.querySelector('[data-color-mode]')?.dataset.colorMode) {
+    case 'dark': {
+      return "dark"
+      break
+    }
+
+    case 'auto': {
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        return "dark"
+        break
+      }
+    }
+
+    case 'light':
+    default: {
+      return "light"
+    }
+  }
+}
+
+let SyntaxTheme = getSyntaxTheme();
+
+const hovercolor = SyntaxTheme == "dark" ? '#f00' : '#00b700';
+
 const additional_css_literal = `
 .uf_badge svg {
   display: table-cell;
   padding-top: 3px;
 }
-tr:hover {background-color: #e2e2e2 !important;}
+tr:hover {background-color: ${hovercolor} !important;}
 tr:nth-child(even) {background-color: #f5f5f5;}
 #${UF_ID_MSG} {color: red}
 `;
@@ -26,11 +92,11 @@ const UF_MSG_ERROR        = "There seems to have been an error while scanning fo
 const UF_MSG_EMPTY_FILTER = "All the forks have been filtered out: you can now rest easy!";
 const UF_MSG_API_RATE     = "<b>Exceeded GitHub API rate-limits.</b>";
 const UF_TABLE_SEPARATOR  = "&nbsp;|&nbsp;";
+const UF_MSG_ACCESS_TOKEN = 'You need to provide a personal Access Token.<br> If you don\'t already have one, you can create one now by clicking <a href="https://github.com/settings/tokens/new?scopes=repo&description=UsefulFork" target="_blank">here</a>';
 
 const FORKS_PER_PAGE = 100; // enforced by GitHub API
 
 let REQUESTS_COUNTER = 0; // to know when it's over
-
 
 function allRequestsAreDone() {
   return REQUESTS_COUNTER <= 0;
@@ -286,10 +352,25 @@ function add_css() {
 }
 
 /** Entry point. */
+function init() {
 const pathComponents = window.location.pathname.split('/');
 if (pathComponents.length >= 3) {
-  const user = pathComponents[1], repo = pathComponents[2];
-  add_css();
-  prepare_display();
-  initial_request(user, repo);
+    if (pathComponents[4] == "members"){
+    const user = pathComponents[1], repo = pathComponents[2];
+      add_css();
+      prepare_display();
+      // only call if GITHUB_ACCESS_TOKEN has been set up
+      if (valid(GITHUB_ACCESS_TOKEN)) {
+        initial_request(user, repo);
+      } else {
+      setMsg(UF_MSG_ACCESS_TOKEN);
+      }
+    }
+  }
 }
+
+init();
+
+document.addEventListener('pjax:end', init);
+
+})();
